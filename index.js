88d991e6121386fffa1c6723912cbd5183641458
Rishijangir566 <rishijangirjr@gmail.com>
2024-10-14 19:11:11 +0530

    complate

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let tempArr = [];
 let countImage =0;
 let pushingImgArr=[];
 let clickCounter=0;
-let timer=0
+let timer=60;
 let matchImg=0;
 
 let start = document.querySelector("#btn");
@@ -12,6 +12,7 @@ let container = document.querySelector("#container");
 let flipFrontImg = document.querySelectorAll(".flip-card-front img");
 let flipCardsBack = document.querySelectorAll(".flip-card-back");
 let Result = document.querySelector("#result");
+let times = document.querySelector("#decreTime");
 
 
 
@@ -33,28 +34,29 @@ let setImage=(() => {
 
 flipFrontImg.forEach(item => {
     item.addEventListener("click",function(){
+        
         countImage++
         clickCounter++
           item.parentElement.parentElement.classList.add("back-flip")
          pushingImgArr.push( item.parentElement.nextElementSibling.children[0])
         //  console.log(pushingImgArr);
          
-         setTimeout(() => {
-             if(countImage>1){
-                if(pushingImgArr[0].src===pushingImgArr[1].src){
+        if(countImage>1){
+            if(pushingImgArr[0].src===pushingImgArr[1].src){
                    pushingImgArr.length=0;
                    countImage=0;
                    matchImg++
                 }
                 else{
+                    setTimeout(() => {
                     pushingImgArr.forEach(items => {
                        items.parentElement.parentElement.classList.remove("back-flip") 
                     });
                     pushingImgArr.length=0;
                    countImage=0;
+                },1000);
                 }
              }
-         },1000);
 
          setInterval(timer)
 
@@ -64,9 +66,9 @@ flipFrontImg.forEach(item => {
    
   function resetGame (){
     let Interval = setInterval(() => {
-        timer++
+       times.innerHTML=--timer
 
-             if(timer===60){
+             if(timer===0){
                  container.style.display="none"
                  Result.style.display="block"
                  let showClick=document.createElement("h2") 
